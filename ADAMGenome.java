88f37ad151ba5ddf872a28b8962590702a107534
neurons <neurons@users.noreply.github.com>
2014-05-23 22:06:12 -0600

    Update ADAMGenome.java
    added attributes, more attributes to add.

--- ADAMGenome.java
+++ ADAMGenome.java
@@ -2,8 +2,22 @@ package org.dishevelled.variation.adam;
 
 public final class ADAMGenome
 {
-//attributes TO-DO
 
-  ADAMGenome(){
-  }
+private final String id;
+private final String description; // ADAM description 
+//attributes remainign to do --> TO-DO
+
+    ADAMGenome(String id, String description){
+      this.id = id;
+      this.description = description;
+    }
+    
+    public String getID(){
+      return id;
+    }
+    
+    public String getDescription()
+    {
+        return description;
+    }
 }
