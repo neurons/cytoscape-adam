2efc8241ab55a93ccacfbd0a27f0585b90f8dd83
neurons <neurons@users.noreply.github.com>
2014-06-01 22:13:02 -0400

    Update ADAMVariationService.java

--- ADAMVariationService.java
+++ ADAMVariationService.java
@@ -1,12 +1,77 @@
+/*
+
+    dsh-variation  Variation.
+    Copyright (c) 2013-2014 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
 package org.dishevelled.variation.adam;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.dishevelled.variation.Feature;
+import org.dishevelled.variation.Variation;
+import org.dishevelled.variation.VariationService;
+
+/**
+ * ADAM file variation service.
+ */
 
 public final class ADAMVariationService implements VariationService
 {
+    private final String species;
+    private final String reference;
+    private final File file; // would this be a
 
-  public VcfVariationService(){ // arguments?
+    public ADAMVariationService(final String species, final String reference, final File file)
+    {
+    checkNotNull(species);
+    checkNotNull(reference);
+    checkNotNull(file);
+    }
 
-  }
+    @Override
+    public List<Variation> variations(final Feature feature)
+    {
+        checkNotNull(feature);
+        checkArgument(species.equals(feature.getSpecies()));
+        checkArgument(reference.equals(feature.getReference()));
 
+        // ick.  re-streams file every time
+        final List<Variation> variations = new ArrayList<Variation>(); // ArrayList of Variation to be returned 
+        try
+        {
+            
+        }
+        catch (IOException e)
+        {
+            // todo
+        }
+        return variations;
+    }
 }
-
-
+}
