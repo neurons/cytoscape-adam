6f2456f1a565cb3dfe3b1c7ddc55554a9dc8b83b
neurons <neurons@users.noreply.github.com>
2014-06-04 21:50:30 -0400

    Update ADAMRecord.java

--- ADAMRecord.java
+++ ADAMRecord.java
@@ -1,110 +1,49 @@
 package org.dishevelled.variation.adam;
 
-import java.util.Map;
 
 /**
 *ADAM Record
 */
 
 public final class ADAMRecord{
-	// Are all of these variables needed from the ADAM format for cytoscape?
-    private int mapq;
-    private String readName;
-    private String sequence;
-    private String mateReference;
-    private long mateAlignmentStart;
-    //private String cigar;   not needed
-    private String qual;
-    private String recordGroupName;
-    private int recordGroupId;
+// include the 4 attributes from ADAMVariant from adam.avdl file.
 
-    private boolean readPaired = false;
-    private boolean properPair = false;
-    private boolean readMapped = false;
-    private boolean mateMapped = false;
-    private boolean readNegativeStrand = false;
-    private boolean mateNegativeStrand = false;
-    private boolean firstOfPair = false;
-    private boolean secondOfPair = false;
-    private boolean primaryAlignment = false;
-    private boolean failedVendorQualityChecks = false;
-    private boolean duplicateRead = false;
+private String contig; //In adam.avdl, there is a struct called ADAMContig; replicate?
+private long position;
+private String referenceAllele;
+private String variantAllele;
 
-    /*private String mismatchingPositions;
-    private String origQual;
-
-    private String attributes;*/
-
-    private String  recordGroupSequencingCenter = null;
-    private String  recordGroupDescription = null;
-    private long recordGroupRunDateEpoch = null;
-    private String  recordGroupFlowOrder = null;
-    private String  recordGroupKeySequence = null;
-    private String recordGroupLibrary = null;
-    private int recordGroupPredictedMedianInsertSize = null;
-    private String  recordGroupPlatform = null;
-    private String  recordGroupPlatformUnit = null;
-    private String  recordGroupSample = null;
-
-    // What about the ADAMContig
-    //union { null, ADAMContig} mateContig = null;
     
-    ADAMRecord(int mapq,
-    	       String readName,
-    	       String sequence,
-    	       String mateReference,
-    	       long mateAlignmentStart,
-    	       String cigar,
-    	       String qual,
-    	       String recordGroupName,
-    	       int recordGroupId
-    		)
+    ADAMRecord(String contig,
+    	       long position,
+    	       String referenceAllele,
+    	       String variantAllele,
+    	      )
     {
-    	this.mapq = mapq;
-    	this.readName = readName;
-    	this.sequence = sequence;
-    	this.mateReference = mateReference;
-    	this.mateAlignmentStart = mateAlignmentStart;
-    	this.cigar = cigar;
-    	this.qual = qual;
-    	this.recordGroupName = recordGroupName;
-    	this.recordGroupId = recordGroupId;
-    	
+	this.contig = contig;
+	this.position = position;
+	this.referenceAllele = referenceAllele;
+	this.variantAllele = variantAllele;
     }
     
-    public int getMapq(){
-    	return mapq;
+    public String getContig()
+    {
+    	return contig;
     }
     
-    public String getReadName(){
-    	return readName;	
+    public long getPosition()
+    {
+    	return position;	
     }
     
-    public String getSequence(){
-    	return sequence;
+    public String getreferenceAllele()
+    {
+    	return referenceAllele;
     }
     
-    public String getMateReference(){
-    	return mateReference;	
+    public String getreferenceAllele()
+    {
+    	return referenceAllele;	
     }
-    
-    public long getMateAlignmentStart(){
-    	mateAlignmentStart;
-    }
-    
-    public String getCigar(){
-    	return cigar;
-    }
-    
-    public String getQual(){
-    	return qual;
-    }
-    
-    public String getRecordGroupName(){
-    	return recordGroupName;
-    }
-    
-    public int getrecordGroupId(){
-    	return recordGroupId;
-    }
+   
 }
